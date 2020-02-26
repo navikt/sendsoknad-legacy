169cf89c2dbdea156d54fecd8cf88f5ba535c5a4
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-02-26 14:45:15 +0100

    SD-1082 - Bumping to Spring 5 and removing dependency

--- web/src/main/java/no/nav/sbl/dialogarena/config/SoknadinnsendingConfig.java
+++ web/src/main/java/no/nav/sbl/dialogarena/config/SoknadinnsendingConfig.java
@@ -1,20 +1,17 @@
 package no.nav.sbl.dialogarena.config;
 
-import no.nav.modig.cache.CacheConfig;
 import no.nav.sbl.dialogarena.soknadinnsending.business.BusinessConfig;
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad.SoknadInnsendingDBConfig;
 import no.nav.sbl.dialogarena.soknadinnsending.consumer.ConsumerConfig;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.*;
 
 @EnableAspectJAutoProxy
 @Configuration
 @Import({
         ApplicationConfig.class,
         BusinessConfig.class,
-        CacheConfig.class,
         ConsumerConfig.class,
         SoknadInnsendingDBConfig.class,
         HandlebarsHelperConfig.class,
@@ -23,4 +20,18 @@ import org.springframework.context.annotation.Import;
 @ComponentScan(basePackages = "no.nav.sbl.dialogarena.rest")
 public class SoknadinnsendingConfig {
 
+    @Bean
+    public EhCacheCacheManager cacheManager() {
+        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+        net.sf.ehcache.CacheManager manager = ehCacheManagerFactoryBean().getObject();
+        cacheManager.setCacheManager(manager);
+        return cacheManager;
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setCacheManagerName("ehCacheManagerFactoryBean");
+        return ehCacheManagerFactoryBean;
+    }
 }
